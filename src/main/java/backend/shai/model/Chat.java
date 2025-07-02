0709071c27f807d8a89210580c28cd62fc428103
Azamat <azamat.rakhim@outlook.com>
2025-07-02 13:36:09 +0500

    Chat model

--- src/main/java/backend/shai/model/Chat.java
+++ src/main/java/backend/shai/model/Chat.java
@@ -2,5 +2,20 @@ package backend.shai.model;
 
 public class Chat {
     private Long id;
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private ChatType type;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "chat_participants",
+            joinColumns = @JoinColumn(name = "chat_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> participants = Collections.synchronizedSet(new HashSet<>());
+
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Message> messages = Collections.synchronizedSet(new HashSet<>());
 
 }
