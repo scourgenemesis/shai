2bba5b2fb2337ccfcc161aa38ec7812c11ec3418
Azamat <azamat.rakhim@outlook.com>
2025-07-02 11:57:27 +0500

    WS config + models

--- src/main/java/backend/shai/config/WebSocketConfig.java
+++ src/main/java/backend/shai/config/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry {
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 }
