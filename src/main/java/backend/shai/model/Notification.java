b3c55d80600605c978032d05e37f95ea0a856455
Azamat <azamat.rakhim@outlook.com>
2025-07-02 14:36:13 +0500

    Additional models

--- src/main/java/backend/shai/model/Notification.java
+++ src/main/java/backend/shai/model/Notification.java
@@ -1,4 +1,47 @@
 package backend.shai.model;
 
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.ManyToOne;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
 public class Notification {
+    @Id @GeneratedValue
+    private Long id;
+    private String content;
+    private boolean isRead;
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User recipient;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
 }
