d6da01647badc2a6d6e2462794a1167002036e84
Azamat <azamat.rakhim@outlook.com>
2025-07-04 16:14:52 +0500

    Model updates

--- src/main/java/backend/shai/model/Attachment.java
+++ src/main/java/backend/shai/model/Attachment.java
@@ -6,10 +6,10 @@ import java.time.LocalDateTime;
 
 public class Attachment {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String fileUrl;
+    private String fileKey;
     private String mimetype;
     private long fileSize;
     private LocalDateTime uploadedAt;
@@ -17,4 +17,49 @@ public class Attachment {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "message_id")
     private Message message;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "uploader_id")
+    private final User uploader;
+
+
+    public Attachment (String filekey, String mimetype, long fileSize, Message message, User uploader) {
+        this.fileKey = filekey;
+        this.mimetype = mimetype;
+        this.fileSize = fileSize;
+        this.message = message;
+        this.uploader = uploader;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFileKey() {
+        return fileKey;
+    }
+
+    public String getMimetype() {
+        return mimetype;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public LocalDateTime getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public User getUploader() {
+        return uploader;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
 }
