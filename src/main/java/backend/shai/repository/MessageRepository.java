1bb610fb4fce3264a0fa7a2fe7299e374dd4b8c1
rkh <azamatrakhim8@gmail.com>
2025-07-08 16:32:46 +0500

    Repository interfaces update

--- src/main/java/backend/shai/repository/MessageRepository.java
+++ src/main/java/backend/shai/repository/MessageRepository.java
@@ -2,11 +2,10 @@ package backend.shai.repository;
 
 
 import backend.shai.model.Message;
-import backend.shai.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends JpaRepository<User, Message> {
+public interface MessageRepository extends JpaRepository<Message, Long> {
 
 }
--- src/main/java/backend/shai/service/UserService.java
+++ src/main/java/backend/shai/service/UserService.java
@@ -1,15 +1,19 @@
 package backend.shai.service;
 
 import backend.shai.model.User;
+import backend.shai.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     @Autowired
-    private User user;
+    private UserRepository userRepo;
 
-    private User createUser(User user) {
+    public User registerUser(User user) {
+        if (userRepo.exists(user.getUsername())) {
 
+        }
     }
+
 }
