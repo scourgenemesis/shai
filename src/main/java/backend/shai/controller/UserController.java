ec5c1207d8e511c6dcb239903b1e9edc1a1ed247
Azamat <azamat.rakhim@outlook.com>
2025-07-09 13:07:05 +0500

    Minor controller update

--- src/main/java/backend/shai/controller/UserController.java
+++ src/main/java/backend/shai/controller/UserController.java
@@ -5,7 +5,9 @@ import backend.shai.model.User;
 import backend.shai.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,8 +22,13 @@ public class UserController {
     }
 
     @PostMapping
-    public User createUser(@RequestBody UserDto userDto) {
-        return userService.createUser(userDto);
+    public User createUser(@RequestBody UserDto userDto, MultipartFile avatarUrl) throws IOException {
+        return userService.registerUser(userDto, avatarUrl);
+    }
+
+    @PostMapping
+    public void updateAvatar(@RequestBody Long userId,MultipartFile avatarUrl) {
+        userService.updateAvatar(userId, String.valueOf(avatarUrl));
     }
 
     @GetMapping("/search")
