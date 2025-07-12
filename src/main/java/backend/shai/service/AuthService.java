2d7a114f6be0fd51e13380f9bc076445665913ae
rkh <azamatrakhim8@gmail.com>
2025-07-13 01:29:55 +0500

    Small AuthService update

--- src/main/java/backend/shai/service/AuthService.java
+++ src/main/java/backend/shai/service/AuthService.java
@@ -1,5 +1,26 @@
 package backend.shai.service;
 
+
+import backend.shai.dto.UserRequest;
+import backend.shai.model.User;
+import backend.shai.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
-    
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtAuthenticationToken jwtAuthenticationToken;
+
+    public User registerUser(UserRequest userRequest) {
+        
+    }
 }
